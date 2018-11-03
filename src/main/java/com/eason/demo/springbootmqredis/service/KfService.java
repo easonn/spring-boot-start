26ae0892300b6a585da4b4b442a25fb07aa37134
eason <yinsheng92@hotmail.com>
2018-11-03 16:42:58 +0800

    init

--- src/main/java/com/eason/demo/springbootmqredis/service/KfService.java
+++ src/main/java/com/eason/demo/springbootmqredis/service/KfService.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
 import java.util.Date;
 
 @Service
-@RabbitListener(queues = {"Kf"})
+@RabbitListener(queues = {"demo1"})
 public class KfService {
 
     @Autowired
